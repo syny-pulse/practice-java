4fe1e5cb6dbf9f873b7eb870e46fd190fb5f4d2a
syny-pulse <sinaijose002@gmail.com>
2023-09-05 22:38:19 +0300

    Progress

---
+++ ex_02.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ex_02 {
+    public static void main (String[] args) throws java.lang.Exception{
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of courses, N:");
+        int N = scanner.nextInt();
+        System.out.println(N*2);
+        scanner.close();
+    }
+
+    
+}
